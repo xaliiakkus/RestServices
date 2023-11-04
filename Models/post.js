19baf6e3f7948606ba42682632409499ed76c980
xaliiakkus <xaliakkus@gmail.com>
2023-11-04 20:39:46 +0300

    add

--- Models/post.js
+++ Models/post.js
@@ -22,6 +22,10 @@ const PostSchema = new mongoose.Schema({
         type: Number,
         default:0
     },
+    price: {
+        type: Number,
+        default:0
+    },
     date: {
         type: Date,
         default:new Date()
